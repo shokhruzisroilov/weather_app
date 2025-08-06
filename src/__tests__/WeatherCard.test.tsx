f5de4af38586fe4479408ed9f4d02c392bf3801f
shokhruzisroilov <shokhruzisroilov@gmail.com>
2025-08-06 17:30:04 +0500

    add fix

--- src/__tests__/WeatherCard.test.tsx
+++ src/__tests__/WeatherCard.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { WeatherCard } from '../components/weather/WeatherCard'
 import '@testing-library/jest-dom'
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: './src/setupTests.ts',
+		setupFiles: './src/__tests__/setup.ts',
 	},
 })
